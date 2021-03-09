e925bd692389896e5962681d154ca0c935263e74
Igor Kosyanenko <barrett.kepler@gmail.com>
2021-03-09 20:13:45 +0300

    aaaaaaa

---
+++ example/states/Gui.cpp
@@ -0,0 +1,4 @@
+//
+// Created by kepler-br on 09.03.2021.
+//
+
---
+++ example/states/Gui.h
@@ -0,0 +1,8 @@
+//
+// Created by kepler-br on 09.03.2021.
+//
+
+#ifndef VANADIUM_GUICONTROLLER_H
+#define VANADIUM_GUICONTROLLER_H
+
+#endif //VANADIUM_GUICONTROLLER_H
