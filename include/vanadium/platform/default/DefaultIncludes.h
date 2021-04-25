ccc5f167cbdbf312c753485a267cd0835bcb4a23
Igor Kosyanenko <barrett.kepler@gmail.com>
2021-04-25 21:05:59 +0300

    Make use of __has_include of c++17 CMakeLists.txt minor changes

--- include/vanadium/platform/default/DefaultIncludes.h
+++ include/vanadium/platform/default/DefaultIncludes.h
@@ -1,6 +1,10 @@
 #ifndef VANADIUM_DEFAULTINCLUDES_H
 #define VANADIUM_DEFAULTINCLUDES_H
 
+#if __has_include(<SDL.h>)
 #include <SDL.h>
+#else
+#include <SDL2/SDL.h>
+#endif
 
 #endif  // VANADIUM_DEFAULTINCLUDES_H
--- include/vanadium/platform/opengl/OpenGLIncludes.h
+++ include/vanadium/platform/opengl/OpenGLIncludes.h
@@ -5,8 +5,8 @@
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/GL3.h>
 #elif defined(VANADIUM_PLATFORM_LINUX)
-#include <GL/gl.h>
 #include <GL/glew.h>
+#include <GL/gl.h>
 #endif
 
 #endif  // VANADIUM_OPENGLINCLUDES_H
